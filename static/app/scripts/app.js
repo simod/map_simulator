3aa9a7b54d98161b0f60ec4284ea6f57aed65ccf
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-03-11 11:56:21 +0100

    configure csrf in home

--- static/app/scripts/app.js
+++ static/app/scripts/app.js
@@ -11,11 +11,5 @@
     'map_controller',
     'feature_controller'    
   ]);
-  // Configure general ajax parameters
-  module.config(function($httpProvider) {
-    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-  });
 })();
 
