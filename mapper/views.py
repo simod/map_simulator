af64155939c6447ca1bb6cb92809c3219178b91c
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-03-11 14:46:11 +0100

    change the kml location

--- mapper/views.py
+++ mapper/views.py
@@ -3,12 +3,13 @@ import json
 from django.template.loader import render_to_string
 from django.views.generic.base import View
 from django.http import HttpResponse
+from django.conf import settings
 
 
 class SerializeKML(View):
     def post(self, request, *args, **kwargs):
         payload = json.loads(request.body)
-        the_kml = open('static/app/data/maps.kml', 'w')
+        the_kml = open(settings.STATIC_ROOT + '/kml/map.kml', 'w')
         the_kml.write(render_to_string('map.kml', {'features': payload['features']['features']}))
         the_kml.close()
         return HttpResponse('ok')
